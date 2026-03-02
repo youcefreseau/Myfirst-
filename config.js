3230dca9c5e7562be99f7d8c8b0ff1f491091df0
youcefreseau <youcefreseau2005@gmail.com>
2026-03-02 16:35:54 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js - هذا الملف سيبقى على جهازك فقط
+const CONFIG = {
+    TELEGRAM_TOKEN: "8766563887:AAF9OB0sZc7YitQRsHQZSUsnyeGLAgnw2fk",  // الصق التوكن الجديد هنا
+    CHAT_ID: "5927792645"  // هذا الرقم لا يتغير
+};
